4e868de3fa70e29569789f087b0ca3de1dd20863
Emily Jirles <emily.jirles@gmail.com>
2018-05-01 14:30:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs 
   @@all = []
+  @@song_count = 0 
   
   def initialize(name)
     @name = name 
@@ -12,6 +13,7 @@ class Artist
   def add_song(song)
     @songs << song 
     song.artist = self 
+    @@song_count += 1 
   end
   
   def add_song_by_name(song_title)
@@ -20,9 +22,7 @@ class Artist
   end
   
   def self.song_count
-   total = 0
-   @@all.each{|artist| total += artist.songs.count}
-   total
+   
   end
   
 end
