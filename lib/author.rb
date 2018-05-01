4c09c154f74175f7563f0653a40c23bb43a39b4d
Emily Jirles <emily.jirles@gmail.com>
2018-05-01 14:32:40 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,11 @@
 class Author 
-  attr_accessor :name 
+  attr_accessor :name, :posts  
   
   @@all = [] 
   
   def initialize(name)
     @name = name 
+    @posts = []
     @@all << self 
   end
   
