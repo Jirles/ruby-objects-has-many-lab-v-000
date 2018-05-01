e91c059ef700f0dd3a8019840742db2ceac20ec2
Emily Jirles <emily.jirles@gmail.com>
2018-05-01 14:39:41 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,7 @@ class Author
   attr_accessor :name, :posts  
   
   @@all = [] 
+  @@post_count = 0 
   
   def initialize(name)
     @name = name 
@@ -12,12 +13,16 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self 
-    #@@song_count += 1 
+    @@post_count += 1 
   end 
   
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     self.add_post(post)
+  end
+  
+  def self.post_count
+    @@post_count
   end
   
 end
